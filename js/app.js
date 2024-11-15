a34a62236a345bc1973b51fcb61bcdfef20c945d
Htet Wunna Aung <htetwanaaung2004@gmail.com>
2024-11-15 20:55:14 +0630

    Portfolio

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// typing Animation 
+var typed = new Typed(".typing", {
+    strings:["Web Developer", "Web Designer", "Student", "Nature Lover"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
